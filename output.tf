8ac703a478e1103e959d0803f3cd7849ed776b2f
kajalp238 <122772368+kajalp238@users.noreply.github.com>
2023-03-26 21:47:21 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.kajal-gurukul.id
+}
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.kajal-gurukul.public_ip
+}
+
+output "private_key" {
+  value     = tls_private_key.private-key.private_key_pem
+  sensitive = true
+}
