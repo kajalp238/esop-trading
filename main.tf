8fa0a24156ffbf4480033435c6fbb0d06b05cb90
kajalp238 <122772368+kajalp238@users.noreply.github.com>
2023-03-26 22:48:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ resource "aws_key_pair" "generated_key" {
 
 }
 
-# resource "local_file" "ssh_key" {
-#   filename = "${aws_key_pair.generated_key.key_name}.pem"
-#   content = tls_private_key.private-key.private_key_pem
-# }
-
 
 data "aws_vpc" "kajal-vpc" {
   id = "vpc-019c09a1a0c5b4f6b"
@@ -39,37 +34,6 @@ resource "aws_subnet" "kajal-subnet" {
     Name = "kajal"
   }
 }
-
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "keypairs"
-#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiyKvzu0oxokTEVuBoudg6ZjKyErY1+vbMWcfTwiv6B8QL9g8gHBzag3KXJ6lg5psVdlLG2QhKzP6wD2E78Tuf7uVTtsFFhvPRrV8NbCOw6VRm5VE68x/a6cAUo7rTbrECHekZE2jRjfcA3jcZ7aTGkHsP5AQHq4WcE1uVvalV8cXCbRJYd3vx3IltjczVIgozgdtWRtIb4hJCsfpwsnOTxb67LMb8C+Rv2aygIRcheduxdVGoBJyiSraLOfLgX90AbvAHV9b85AZ5CKuW/dl3WFP7stj4+PNGSbc/pyUjLPxhK8OplyyNhgT+x5kJO9LNlygrX321aLzab5179nLr kajalp@Kajals-MacBook-Pro.local"
-# }
-
-
-# resource "aws_s3_bucket" "kajal-gurukul-bucket" {
-#   bucket = "kajal-gurukul-bucket"
-
-#   versioning {
-#     enabled = true
-#   }
-
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
-
-# data "terraform_remote_state" "network" {
-#   backend = "s3"
-#   config = {
-#     bucket = "kajal-gurukul-bucket"
-#     key    = "s3/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
 
 
 resource "aws_instance" "kajal-gurukul" {
