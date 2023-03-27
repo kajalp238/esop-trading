3c27dea247caf712e22c0e8dc32638fac809227d
kajalp238 <122772368+kajalp238@users.noreply.github.com>
2023-03-27 12:35:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,12 +7,4 @@ output "instance_public_ip" {
   value       = aws_instance.kajal-gurukul.public_ip
 }
 
-output "private_key" {
-  value     = tls_private_key.private-key.private_key_pem
-  sensitive = true
-}
 
-output "instance_dns" {
-  description = "Public dns"
-  value       = aws_instance.kajal-gurukul.public_dns
-}
