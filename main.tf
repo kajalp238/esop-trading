39babaa71c4f850959c45e3ccb732288cc06e2fd
kajalp238 <kajalp@sahaj.ai>
2023-03-27 15:01:03 +0530

    security group add

--- main.tf
+++ main.tf
@@ -81,6 +81,17 @@ resource "aws_security_group" "kajal-gurukul-sg" {
       security_groups  = []
       self             = false
       to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 8080
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 8080
     }
   ]
 }
